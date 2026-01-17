f7fae2500bdc842d0cad6345bfffd8376c7929aa
Garthcodex <garthroach@gmail.com>
2026-01-17 14:45:23 -0500

    Update script integrity hash in aeo.js
    “Replace placeholder with Codex AEO v1 snippet”

--- aeo/v1/aeo.js
+++ aeo/v1/aeo.js
@@ -1,1 +1,1 @@
-<script src="https://staging.app.codex-aeo-co.com/aeo/v1/aeo.js" integrity="sha256-PASTE_HASH_HERE" crossorigin="anonymous" async></script>
+<script src="https://staging.app.codex-aeo-co.com/aeo/v1/aeo.js" integrity="sha256-aZiGETjxkCV0guNdP+4/Hw0nWXPyX+0YxkDlmsxM0+4=" crossorigin="anonymous" async></script>
